26ead20f4dcc73b7c0e4c8659fd0db83885e4b5f
Jonah <jonah11104@gmail.com>
2020-12-08 09:06:55 -0500

    goal detection vision added to branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoalDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoalDetector.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
@@ -61,28 +63,31 @@ public class GoalDetector extends LinearOpMode
      * particularly useful during pipeline development. We also show how
      * to get data from the pipeline to your OpMode.
      */
-    static class StageSwitchingPipeline extends OpenCvPipeline
-    {
-        Mat yCbCrChan1Mat = new Mat();
+
+
+
+
+    static class StageSwitchingPipeline extends OpenCvPipeline {
+        Mat yCbCrChan2Mat = new Mat();
         Mat thresholdMat = new Mat();
         Mat contoursOnFrameMat = new Mat();
         List<MatOfPoint> contoursList = new ArrayList<>();
         int numContoursFound;
 
-        enum Stage
-        {
+        enum Stage {
             YCbCr_CHAN2,
             THRESHOLD,
             CONTOURS_OVERLAYED_ON_FRAME,
             RAW_IMAGE,
         }
 
+        static final Scalar TEAL = new Scalar(128, 0, 128);
+
         private Stage stageToRenderToViewport = Stage.YCbCr_CHAN2;
         private Stage[] stages = Stage.values();
 
         @Override
-        public void onViewportTapped()
-        {
+        public void onViewportTapped() {
             /*
              * Note that this method is invoked from the UI thread
              * so whatever we do here, we must do quickly.
@@ -92,8 +97,7 @@ public class GoalDetector extends LinearOpMode
 
             int nextStageNum = currentStageNum + 1;
 
-            if(nextStageNum >= stages.length)
-            {
+            if (nextStageNum >= stages.length) {
                 nextStageNum = 0;
             }
 
@@ -101,53 +105,80 @@ public class GoalDetector extends LinearOpMode
         }
 
         @Override
-        public Mat processFrame(Mat input)
-        {
+        public Mat processFrame(Mat input) {
             contoursList.clear();
 
-            /*
-             * This pipeline finds the contours of yellow blobs such as the Gold Mineral
-             * from the Rover Ruckus game.
-             */
-            Imgproc.cvtColor(input, yCbCrChan1Mat, Imgproc.COLOR_RGB2YCrCb);
-            Core.extractChannel(yCbCrChan1Mat, yCbCrChan1Mat, 1);
-            Imgproc.threshold(yCbCrChan1Mat, thresholdMat, 102, 255, Imgproc.THRESH_BINARY);
+            Imgproc.cvtColor(input, yCbCrChan2Mat, Imgproc.COLOR_RGB2YCrCb);
+            Core.extractChannel(yCbCrChan2Mat, yCbCrChan2Mat, 1);
+            Imgproc.threshold(yCbCrChan2Mat, thresholdMat, 160, 255, Imgproc.THRESH_BINARY);
             Imgproc.findContours(thresholdMat, contoursList, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
             numContoursFound = contoursList.size();
             input.copyTo(contoursOnFrameMat);
-            Imgproc.drawContours(contoursOnFrameMat, contoursList, -1, new Scalar(0, 0, 255), 3, 8);
+//        Imgproc.drawContours(contoursOnFrameMat, contoursList, -1, new Scalar(0, 0, 255), 3, 8);
+            List<Rect> contourRects = new ArrayList();
+            for (Mat contour : contoursList) {
+                double area = Imgproc.contourArea(contour);
+                double sqrtArea = Math.sqrt(area);
+                System.out.printf("Area: %f\n", area);
 
-            switch (stageToRenderToViewport)
-            {
-                case YCbCr_CHAN2:
-                {
-                    return yCbCrChan1Mat;
+                //input.cols()/2 - xcenter
+                //input.rows()/2 - ycenter
+
+                if (sqrtArea > 25 && sqrtArea < 45) {
+                    contourRects.add(Imgproc.boundingRect(contour));
+                }
+            }
+
+
+            double lastArea = 0;
+            Rect chosen = null;
+            for (Rect r : contourRects) {
+                if (r.width * r.height > lastArea) {
+                    chosen = r;
+                    lastArea = r.width * r.height;
+                }
+            }
+
+            Imgproc.rectangle(contoursOnFrameMat, chosen, new Scalar(255, 0, 0), 2);
+            double halfwayPoint = chosen.x + .5 * chosen.width;
+            double centerOffset = input.cols() / 2 - halfwayPoint;
+            Imgproc.putText(
+                    contoursOnFrameMat,
+                    Integer.toString((int) centerOffset),
+                    new Point(// The anchor point for the text
+                            30,  // x anchor point
+                            50), // y anchor point
+                    Imgproc.FONT_HERSHEY_PLAIN, // Font
+                    3, // Font size
+                    TEAL, // Font color
+                    6); // Font thickness));
+
+
+            switch (stageToRenderToViewport) {
+                case YCbCr_CHAN2: {
+                    return yCbCrChan2Mat;
                 }
 
-                case THRESHOLD:
-                {
+                case THRESHOLD: {
                     return thresholdMat;
                 }
 
-                case CONTOURS_OVERLAYED_ON_FRAME:
-                {
+                case CONTOURS_OVERLAYED_ON_FRAME: {
                     return contoursOnFrameMat;
                 }
 
-                case RAW_IMAGE:
-                {
+                case RAW_IMAGE: {
                     return input;
                 }
 
-                default:
-                {
+                default: {
                     return input;
                 }
             }
         }
 
-        public int getNumContoursFound()
-        {
+
+        public int getNumContoursFound() {
             return numContoursFound;
         }
     }
